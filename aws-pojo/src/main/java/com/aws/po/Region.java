d4934215fd7980c79f8d934c86ed83bb38fb7b0a
TobiichiOrigamii <origami9103@gmail.com>
2025-07-09 12:27:39 +0800

    feat(Region): 获取所有区域信息
    - 增加了获取所有区域信息的功能

--- aws-pojo/src/main/java/com/aws/po/Region.java
+++ aws-pojo/src/main/java/com/aws/po/Region.java
@@ -5,23 +5,53 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  * @author Origami
  * @description 区域实体类
  * @create 2025-07-09 11:01
  **/
+/**
+ * 风电区域信息表
+ * @TableName region
+ */
 @Data
 @Builder
-@AllArgsConstructor
 @NoArgsConstructor
-public class Region {
+@AllArgsConstructor
+public class Region implements Serializable {
+
+    /**
+     * 区域主键ID
+     */
     private Long id;
+    /**
+     * 区域名称
+     */
     private String regionName;
+    /**
+     * 区域编码（唯一标识）
+     */
     private String regionCode;
-    private Double latitude;
-    private Double longitude;
+    /**
+     * 区域中心点纬度
+     */
+    private BigDecimal latitude;
+    /**
+     * 区域中心点经度
+     */
+    private BigDecimal longitude;
+    /**
+     * 备注信息
+     */
     private String remark;
-    private LocalDateTime createTime;
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
 }
