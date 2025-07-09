efb9e6e3c62fb97efd5838db8a2d9aef414b95aa
xuchangyyds <3156324169@qq.com>
2025-07-09 10:05:23 +0800

    初始工程

---
+++ aws-server/src/main/java/com/aws/AwsApplication.java
@@ -0,0 +1,11 @@
+package com.aws;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(AwsApplication.class, args);
+    }
+}
